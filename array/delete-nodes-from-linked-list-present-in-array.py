b9a3e41ac3b4f95dc7e3fb5b56cc76a10638216c
Joseph Oduyebo <69268411+jayco12@users.noreply.github.com>
2024-10-05 07:27:42 +0100

    LC-delete-nodes-from-linked-list-present-in-array [Runtime: 867 ms; Memory: 65.5 MB]

--- array/delete-nodes-from-linked-list-present-in-array.py
+++ array/delete-nodes-from-linked-list-present-in-array.py
@@ -5,14 +5,14 @@
 #         self.next = next
 class Solution:
     def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
-        
+        nums_set = set(nums)
         j=ListNode(0)
         
         lis=j
         new=head
         while new:
             
-            if new.val not in nums:
+            if new.val not in nums_set:
                 lis.next = ListNode(new.val)  
                 lis = lis.next 
             new=new.next
