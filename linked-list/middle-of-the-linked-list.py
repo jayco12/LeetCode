ea6d69266bea6e6ddf441a56ff95c78078175165
Joseph Oduyebo <69268411+jayco12@users.noreply.github.com>
2024-11-12 01:39:31 +0100

    LC-middle-of-the-linked-list [Runtime: 0 ms; Memory: 16.6 MB]

--- linked-list/middle-of-the-linked-list.py
+++ linked-list/middle-of-the-linked-list.py
@@ -6,26 +6,13 @@
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
-        current=head
-        count=0
-            
-        while current:
-            count+=1
-            # prev=current
-            current=current.next
-        # if count%2==0:
-        middle=count//2+1
-            # print(middle)
-        # elif count%2!=0: # count = 6
-        #     middle=(count//2)+1 # count // 2 + 1
-            # print(middle)
+        slow=head
+        fast=head
 
-        current=head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
 
-        for _ in range(0, int(middle-1)):
-            current=current.next
-            # print(current.value)
-            
-        return current
+        return slow
             
         
