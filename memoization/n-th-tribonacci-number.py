5c5d0ae38c0cce22e04a9de9fd86e0960abf7ebc
Joseph Oduyebo <69268411+jayco12@users.noreply.github.com>
2024-11-18 00:58:23 +0100

    LC-n-th-tribonacci-number [Runtime: N/A; Memory: N/A]

--- memoization/n-th-tribonacci-number.py
+++ memoization/n-th-tribonacci-number.py
@@ -2,6 +2,8 @@ class Solution:
     def tribonacci(self, n: int) -> int:
         if n < 1:
             return 0
+        if n = 1:
+            return 1
         ans = [None] * (n + 1)
 
         ans[0] = 0
