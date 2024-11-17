581a9e9d5115b8a20b6ad06927c99f5ed68c4c26
Joseph Oduyebo <69268411+jayco12@users.noreply.github.com>
2024-11-17 23:38:16 +0100

    LC-climbing-stairs [Runtime: 0 ms; Memory: 16.7 MB]

--- memoization/climbing-stairs.py
+++ memoization/climbing-stairs.py
@@ -5,14 +5,8 @@ class Solution:
         def climb_helper(i: int) -> int:
             if i <= 1:
                 return 1
-            
-            # check if value is already in cache
-            # before making recursive calls
-            # corresponds to the green nodes in the diagram
             if i in memo:
                 return memo[i]
-            
-            # store result in cache before returning
             memo[i] = climb_helper(i - 1) + climb_helper(i - 2)
             return memo[i]
         
