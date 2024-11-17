8fc5d7e4fe0fe5f42b91c479179f2272c77525fc
Joseph Oduyebo <69268411+jayco12@users.noreply.github.com>
2024-11-17 23:37:59 +0100

    LC-climbing-stairs [Runtime: 0 ms; Memory: 16.7 MB]

--- math/climbing-stairs.py
+++ math/climbing-stairs.py
@@ -1,6 +1,19 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n <= 1:
-            return 1
+        memo = {}
     
-        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        def climb_helper(i: int) -> int:
+            if i <= 1:
+                return 1
+            
+            # check if value is already in cache
+            # before making recursive calls
+            # corresponds to the green nodes in the diagram
+            if i in memo:
+                return memo[i]
+            
+            # store result in cache before returning
+            memo[i] = climb_helper(i - 1) + climb_helper(i - 2)
+            return memo[i]
+        
+        return climb_helper(n)
