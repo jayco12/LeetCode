35576ed097dbcbb65619c306aa7a4cbb7ccc318e
Joseph Oduyebo <69268411+jayco12@users.noreply.github.com>
2024-12-02 19:29:00 -0600

    LC-keep-multiplying-found-values-by-two [Runtime: N/A; Memory: N/A]

---
+++ sorting/keep-multiplying-found-values-by-two.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        seen=set()
+        for num in nums:
+            seen.add(num)
+            if original in seen:
+                original=original*2
+           
+        return original
