7f053ee6fd2aec43681a0cab8e16fa637c8ff4e1
Joseph Oduyebo <69268411+jayco12@users.noreply.github.com>
2024-12-05 12:17:24 -0600

    LC-find-if-path-exists-in-graph [Runtime: N/A; Memory: N/A]

--- union-find/find-if-path-exists-in-graph.py
+++ union-find/find-if-path-exists-in-graph.py
@@ -1,7 +1,10 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
       
-        
+        if n==0:
+            return True
+        if n==1:
+            return True
         for i in range(len(edges)):
             for j in range(len(edges[i])):
                 if edges[i][j]==source and edges[i][j-1]==destination:
