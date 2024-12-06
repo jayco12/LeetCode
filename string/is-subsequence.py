88e571ddb90ad10c131228becd48add6101ec85f
Joseph Oduyebo <69268411+jayco12@users.noreply.github.com>
2024-12-06 17:38:16 -0600

    LC-is-subsequence [Runtime: 3 ms; Memory: 17.1 MB]

--- string/is-subsequence.py
+++ string/is-subsequence.py
@@ -1,8 +1,12 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        seen=set(t)
+        seen = set(t)  
+        t_index = 0  # Pointer for `t`
+        s_index = 0  # Pointer for `s`
 
-        for i in range(len(s)):
-            if s[i] not in seen:
-                return False
-        return True
+        # Traverse through `t` while checking characters in `s`
+        while t_index < len(t) and s_index < len(s):
+            if t[t_index] == s[s_index]:  # Match found
+                s_index += 1  # Move `s` pointer to check the next character
+            t_index += 1 
+        return s_index == len(s)
