bc87f3424c2e876dca0c193a02a278e23914584c
Joseph Oduyebo <69268411+jayco12@users.noreply.github.com>
2024-12-06 17:41:13 -0600

    LC-is-subsequence [Runtime: 0 ms; Memory: 17.5 MB]

--- string/is-subsequence.py
+++ string/is-subsequence.py
@@ -1,8 +1,10 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         seen = set(t)  
-        t_index = 0  
+       
         s_index = 0 
+        if len(s)==0:
+            return True
         for char in t:  # Traverse through `t`
             if s_index < len(s) and char == s[s_index]:  # Check order and match
                 s_index += 1
