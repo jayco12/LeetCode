581efef143305f84a1d815a14623905fa7f11a84
Joseph Oduyebo <69268411+jayco12@users.noreply.github.com>
2024-12-10 22:10:49 -0600

    LC-reverse-integer [Runtime: 29 ms; Memory: 17.2 MB]

--- math/reverse-integer.py
+++ math/reverse-integer.py
@@ -1,17 +1,19 @@
 class Solution:
     def reverse(self, x: int) -> int:
         x=str(x)
+        min_int = -2**31
+        max_int = 2**31 - 1
         
 
         if x[0]=="-":
             s="-"
             for i in range(len(x)-1, 0, -1):
                 s+=x[i]
+            if int(s)< min_int or int(s) > max_int:
+        
+                return 0
             return int(s)
-        min_int = -2**31
-        max_int = 2**31 - 1
-
-        # Check if the result is within the 32-bit range
+        
         if int(x[::-1])< min_int or int(x[::-1]) > max_int:
         
             return 0
