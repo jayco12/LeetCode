23a8dad4e95a30050b6937a30a3833592569d7f7
Joseph Oduyebo <69268411+jayco12@users.noreply.github.com>
2024-12-12 14:27:27 -0600

    LC-take-gifts-from-the-richest-pile [Runtime: 67 ms; Memory: 17.4 MB]

--- array/take-gifts-from-the-richest-pile.py
+++ array/take-gifts-from-the-richest-pile.py
@@ -1,12 +1,10 @@
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
-        gifts=sorted(gifts)
-        print(gifts)
-        remainder=[]
-        s=0
-        for i in range(len(gifts)):
-            remainder.append(math.sqrt(gifts[i]))
-        for j in range(len(remainder)-k, len(remainder)):
-            s+=remainder[j]
-            print(remainder[j])
-        return int(s)
+        
+        for _ in range(k):
+            max_val=max(gifts)
+            max_index=gifts.index(max_val)
+
+
+            gifts[max_index]=floor(math.sqrt(max_val))
+        return int(sum(gifts))
