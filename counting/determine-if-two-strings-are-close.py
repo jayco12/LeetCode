aabbe9cb569b2be05ce4d090c6c37471dc552aea
Joseph Oduyebo <69268411+jayco12@users.noreply.github.com>
2024-12-12 22:21:10 -0600

    LC-determine-if-two-strings-are-close [Runtime: N/A; Memory: N/A]

--- counting/determine-if-two-strings-are-close.py
+++ counting/determine-if-two-strings-are-close.py
@@ -1,11 +1,11 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
         seen=set(word1)
+        seen2=set(word2)
         
         if len(word1)!=len(word2):
             return False
 
-        for i in range(len(word2)):
-            if word1[i] not in seen:
-                return False
+        if seen==seen2:
             return True
+        return False
