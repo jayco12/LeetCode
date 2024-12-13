62dfd6ee5a8bd86ab08b144a37895dd9b5c6d522
Joseph Oduyebo <69268411+jayco12@users.noreply.github.com>
2024-12-12 22:18:59 -0600

    LC-determine-if-two-strings-are-close [Runtime: N/A; Memory: N/A]

--- sorting/determine-if-two-strings-are-close.py
+++ sorting/determine-if-two-strings-are-close.py
@@ -1,6 +1,9 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
         seen=set(word2)
+        
+        if len(word1)!=len(word2):
+            return False
 
         for i in range(len(word1)):
             if word1[i] not in seen:
