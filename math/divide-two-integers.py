fbc547184b8b20cfe24da24bef1019724bb0cdc8
Joseph Oduyebo <69268411+jayco12@users.noreply.github.com>
2024-12-24 11:05:54 -0600

    LC-divide-two-integers [Runtime: N/A; Memory: N/A]

--- math/divide-two-integers.py
+++ math/divide-two-integers.py
@@ -1,3 +1,12 @@
+import math
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        return round(dividend/divisor)
+        INT_MAX = 2**31 - 1 
+        INT_MIN = -2**31   
+
+        if dividend == INT_MIN and divisor == -1:
+            return INT_MAX
+
+        result = round(dividend / divisor)
+
+        return result
