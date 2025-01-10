c3151fbfd043e7482d90d67358381f267f854c1e
Joseph Oduyebo <69268411+jayco12@users.noreply.github.com>
2025-01-10 11:06:46 -0600

    LC-word-subsets [Runtime: N/A; Memory: N/A]

--- array/word-subsets.py
+++ array/word-subsets.py
@@ -3,11 +3,11 @@ class Solution:
         stack = [] 
         for word in words1:
             stack.append(word)
-
+        words = "".join(words2) 
         result_list = [] 
 
         while stack:
             word = stack.pop()  
-            if all(element in word for element in words2):  
+            if all(char in word for char in words):  
                 result_list.append(word) 
         return result_list
