83d81592a09410d9dd5cc19523f410053720a67c
Joseph Oduyebo <69268411+jayco12@users.noreply.github.com>
2025-02-04 13:36:04 -0600

    LC-valid-sudoku [Runtime: N/A; Memory: N/A]

--- matrix/valid-sudoku.py
+++ matrix/valid-sudoku.py
@@ -4,15 +4,21 @@ class Solution:
        
         rows=len(board)
         cols=len(board[0])
+        boxes = defaultdict(set)
         seen={}
         row_seen = [set() for _ in range(rows)]
         col_seen = [set() for _ in range(cols)]
         
-        for row in range(rows):
-            for col in range(cols):
-                if board[row][col] in row_seen[row] or board[row][col] in col_seen[col]:
+        for row in range(9):
+            for col in range(9):
+                if board[row][col] == ".":
+                    continue
+                if board[row][col] in row_seen[row] or board[row][col] in col_seen[col] or board[row][col] in boxes[(row // 3, col // 3)]:
                     return False
                 row_seen[row].add(col)
                 col_seen[col].add(row)
+                boxes[(row // 3, col // 3)].add(board[row][col])
+        
+
     
         return True
