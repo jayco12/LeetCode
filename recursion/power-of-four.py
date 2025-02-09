158d39270f03613b62522bede5ea1cdc75f568b3
Joseph Oduyebo <69268411+jayco12@users.noreply.github.com>
2025-02-09 16:24:32 -0600

    LC-power-of-four [Runtime: N/A; Memory: N/A]

--- recursion/power-of-four.py
+++ recursion/power-of-four.py
@@ -2,6 +2,9 @@ class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         if n==1:
             return True
+        x=str(n)
+        if x[0]=="-":
+            return False
         if n%4==0:
             return True
         return False
